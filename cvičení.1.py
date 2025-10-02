91d70d3938c1ccf94d521842fdd91c564caab849
JanHouser <jan007jan007@seznam.cz>
2025-10-02 08:58:22 +0000

    Add initial implementation of delitelne_beze_zbytku_3 function and create new files

---
+++ cvičení.1.py
@@ -0,0 +1,10 @@
+
+def delitelne_beze_zbytku_3(cislo):
+    return False
+
+    
+if _name'__name__' == "_main_":
+    if delitelne_beze_zbytku_3(3):
+      print("Ano, je dělitelné beze zbytku")
+    else:
+       print("Ne, není dělitelné beze zbytku")
