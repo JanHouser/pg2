4b92ffe1daec76c8842654a300e90b29dc443108
JanHouser <jan007jan007@seznam.cz>
2025-10-30 10:15:51 +0000

    hotg

--- 301.py
+++ 301.py
@@ -1,3 +1,7 @@
+import sys
+import csv
+
+
 def precti_hodnoty_a_incrementuj(file):
 
     all_results = []
@@ -29,3 +33,47 @@ def zapis_data_do_csv(file_name, data):
         for row in data:
             line = ','.join(row)
     data = precti_hodnoty_a_incrementuj(file)        
+
+
+
+
+def nacti_csv(soubor):
+    data = []
+    fp = open(soubor)
+    reader = csv.reader(fp)
+    for row in reader:
+        data.append(row)
+    fp.close()
+    return data
+
+
+def spoj_data(data1, data2):
+    print(data1)
+    print(data2)
+    header1 = data1[0]
+    header2 = data2[0]
+    header = set()
+    header.update(header1)
+    header.update(header2)
+    print (header)
+
+
+def zapis_csv(soubor, data):
+    with open(soubor, "w") as fp:
+        writer = csv.writer(fp)
+        writer.writerows(data)
+
+
+if __name__ == "__main__":
+    try:
+        soubor1 = sys.argv[1]
+        soubor2 = sys.argv[2]
+        csv_data1 = nacti_csv(soubor1)
+        csv_data2 = nacti_csv(soubor2)
+        vysledna_data = spoj_data(csv_data1, csv_data2)
+        print(vysledna_data)
+        zapis_csv("vysledny_excel.csv", vysledna_data)
+    except IndexError:
+        print("Nebyly zadany soubory")
+    except FileNotFoundError:
+        print("Soubor neexistuje")
